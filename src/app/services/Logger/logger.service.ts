84b0bebb9e0f0bb8a2e2a400f9090a954f1da209
chandra pothireddy <venky.vr.8099@gmail.com>
2024-10-03 00:12:51 +0530

    10. Write Test Case for the Angular Pipe using Isolated Unit Tests - Angular

--- src/app/services/Logger/logger.service.ts
+++ src/app/services/Logger/logger.service.ts
@@ -5,12 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class LoggerService {
   constructor() {
-    debugger; // to see how many times it is calling
   }
   messages: string[] = [];
 
   log(message: string) {
-    debugger; // to see how many times it is calling
     this.messages.push(message);
   }
 }
