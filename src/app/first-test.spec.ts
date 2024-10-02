3363a4cb42301c878a551b5f64ab65c52ea9a2c0
chandra pothireddy <venky.vr.8099@gmail.com>
2024-10-02 12:27:28 +0530

    6. Step by Step implementation of the Calculator Service with Jasmine Specification - Angular

--- src/app/first-test.spec.ts
+++ src/app/first-test.spec.ts
@@ -1,13 +1,17 @@
-describe('First test', () => {
+describe('First Test', () => {
   let testVariable: any;
+
   beforeEach(() => {
     testVariable = {};
   });
+
   it('should return true if a is true', () => {
-    // arrange
+    //arrange
     testVariable.a = false;
+
     //act
     testVariable.a = true;
+
     //assert
     expect(testVariable.a).toBe(true);
   });
