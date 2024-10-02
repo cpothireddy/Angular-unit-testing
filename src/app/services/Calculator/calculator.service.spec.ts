31822204fa07334981b15f27b7a5303e69aa61dc
chandra pothireddy <venky.vr.8099@gmail.com>
2024-10-03 00:00:18 +0530

    9. Using BeforeEach setup method to structure the Angular Test Cases for avoiding Repetitive code

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -2,22 +2,23 @@ import { LoggerService } from '../Logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
+  let mockLoggerService: any;
+  let calculator: CalculatorService;
+  beforeEach(() => {
+    console.log('calling before each');
+    mockLoggerService = jasmine.createSpyObj('chandraService', ['log']);
+    calculator = new CalculatorService(mockLoggerService);
+  });
+  // beforeEach will run before every test case
   it('should add two numbers', () => {
-    // let loggerService = new LoggerService();
-    // now, I want to create a spy on complete service, that means I don't want to hit the original service, instead I will create a spyon object on the loggerService.
-    let mockLoggerService = jasmine.createSpyObj('chandraService', ['log']);
-    // in above line, we are creating(spyonObj) a service and log method, that will use in below add method.
-    // you can give any name here, but we are matching that with logService in below line
-    const calculator = new CalculatorService(mockLoggerService);
-    // spyOn(loggerService, 'log');
+    console.log('calling add method');
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(mockLoggerService);
+    console.log('calling subtract method');
     let result = calculator.subtract(2, 2);
     expect(result).toBe(0);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
