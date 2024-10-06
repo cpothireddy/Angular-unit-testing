0a64757ac013d40aec8550d88b367340747ee24d
chandra pothireddy <venky.vr.8099@gmail.com>
2024-10-06 17:36:56 +0530

    30. Testing Services which has HttpClient as dependency by using Jasmine Spy - Angular Unit testing

---
+++ src/app/services/Post/post.service.spec.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { PostService } from './post.service';
+import { of } from 'rxjs';
+
+describe('post Service', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let postService: PostService;
+  let POSTS = [
+    {
+      id: 1,
+      body: 'body 1',
+      title: 'title 1',
+    },
+    {
+      id: 2,
+      body: 'body 2',
+      title: 'title 2',
+    },
+    {
+      id: 3,
+      body: 'body 3',
+      title: 'title 3',
+    },
+  ];
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    postService = new PostService(httpClientSpy);
+  });
+  describe('getPosts()', () => {
+    it('should return expected posts when getPosts is called', (done: DoneFn) => {
+      httpClientSpy.get.and.returnValue(of(POSTS));
+      postService.getPosts().subscribe({
+        next: (posts) => {
+          expect(posts).toEqual(POSTS);
+          done(); // it is saying that this test case will finish once we can done method here
+        },
+        error: () => {
+          done.fail;
+        },
+      });
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+    });
+  });
+});
